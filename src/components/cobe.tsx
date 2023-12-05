2e001e85ea2cce8a7ffc8d5f9cfbe88bea6b8994
Ninn04 <lynin722@gmail.com>
2023-12-05 07:55:36 +0700

    uodate

--- src/components/cobe.tsx
+++ src/components/cobe.tsx
@@ -1,15 +1,24 @@
 "use client"
 import { Canvas, useLoader } from "@react-three/fiber"
-import { Mesh } from 'three'
+import { TextureLoader } from 'three'
 
 const Cobe = () => {
+
+  const [color, normal, aoMap] = useLoader(TextureLoader, [
+    "/public/16 amazing photos of Earth pinned from space by astronaut Karen Nyberg.jpeg",
+    "/public/download (1).jpeg",
+    "/public/download.jpeg"
+  ])
+
   return (
     <div className="flex justify-center items-center">
         <Canvas>
-            <mesh scale={2.5}>
-                <sphereGeometry args={[1,32,32]} />
-                <meshStandardMaterial color="red" />
-            </mesh>
+          <ambientLight intensity={0.1} />
+          <directionalLight intensity={3.5} position={[1, 0, -.25]} />
+          <mesh scale={2.5}>
+              <sphereGeometry args={[1,32,32]} />
+              <meshStandardMaterial map = {color} />
+          </mesh>
         </Canvas>
     </div>
   )
